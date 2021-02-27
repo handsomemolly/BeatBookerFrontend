6fcc9628ee48ff3d35488a20db642e7a9d6b44e9
handsomemolly <ryan.kendig@gmail.com>
2021-02-27 10:40:57 -0800

    weird cards are on the screen

--- beatbooker-client/src/components/Home.js
+++ beatbooker-client/src/components/Home.js
@@ -15,10 +15,27 @@ class Home extends Component {
       });
   }
 
+  generateArtists = () => {
+    return this.state.artists.map((artist) => {
+      return (
+        <div className="card">
+          <div className="cardContainer">
+            <ul>
+              <h2>{artist.name}</h2>
+              <p>Type: {artist.artist_type}</p>
+              <p>Price per Event: ${artist.price}</p>
+            </ul>
+          </div>
+        </div>
+      );
+    });
+  };
+
   render() {
     return (
       <div>
         <h1 className="title">Book Your Next Beat</h1>
+        {this.generateArtists()}
       </div>
     );
   }
