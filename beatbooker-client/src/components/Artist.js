f50b18539772acd6d9d6613a627d040b8d5c808c
handsomemolly <ryan.kendig@gmail.com>
2021-02-28 17:30:45 -0800

    artist page rendering

--- beatbooker-client/src/components/Artist.js
+++ beatbooker-client/src/components/Artist.js
@@ -4,12 +4,21 @@ import "../App.css";
 
 class Artist extends Component {
   generateArtistInfo = () => {
-    return this.props.artists.map;
+    return this.props.artists.map((artist) => {
+      return (
+        <article className="artist-page">
+          <h1>{artist.name}</h1>
+          <p>Bio: {artist.bio}</p>
+          <p>{artist.video_url}</p>
+        </article>
+      );
+    });
   };
   render() {
     return (
       <div>
         <h1>Artist Page</h1>
+        <section>{this.generateArtistInfo()}</section>
       </div>
     );
   }
