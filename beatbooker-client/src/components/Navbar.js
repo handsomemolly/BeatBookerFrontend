c4e2f20f127e43de17d60be60e76f72dc32dec56
handsomemolly <ryan.kendig@gmail.com>
2021-03-02 16:47:52 -0800

    navigation update

--- beatbooker-client/src/components/Navbar.js
+++ beatbooker-client/src/components/Navbar.js
@@ -2,41 +2,53 @@ import React from "react";
 import { NavLink } from "react-router-dom";
 const Navbar = ({ user, handleLogout }) => {
   return (
-    <header className="topNav">
-      <h3 className="navTitle">BeatBooker</h3>
-      <h4>
-        {!user.id ? (
-          <>
-            <p>
-              <NavLink to="/login">Login</NavLink>
-            </p>
-            <p>
-              <NavLink to="/signup">Sign up</NavLink>
-            </p>
-            <p>
-              <NavLink to="/bookings">My Bookings</NavLink>
-            </p>
-            <p>
-              <NavLink to="/home">Home</NavLink>
-            </p>
-          </>
-        ) : (
-          <>
-            <p>
-              <NavLink to="/home" exact>
-                Home
-              </NavLink>
-            </p>
-            <p>
-              <NavLink to="/users">Users</NavLink>
-            </p>
-            <p>
-              <button onClick={handleLogout}>Logout</button>
-            </p>
-          </>
-        )}
-      </h4>
-    </header>
+    <div>
+      <div className="nav">
+        <header className="topNav">
+          <h3 className="navTitle">BeatBooker</h3>
+          <ul>
+            {!user.id ? (
+              <>
+                <li>
+                  <a href="#" className="nav-link">
+                    <NavLink to="/login">Login</NavLink>
+                  </a>
+                </li>
+                <li>
+                  <a href="#" className="nav-link">
+                    <NavLink to="/signup">Sign up</NavLink>
+                  </a>
+                </li>
+                <li>
+                  <a href="#" className="nav-link">
+                    <NavLink to="/bookings">My Bookings</NavLink>
+                  </a>
+                </li>
+                <li>
+                  <a href="#" className="nav-link">
+                    <NavLink to="/home">Home</NavLink>
+                  </a>
+                </li>
+              </>
+            ) : (
+              <>
+                <p>
+                  <NavLink to="/home" exact>
+                    Home
+                  </NavLink>
+                </p>
+                <p>
+                  <NavLink to="/users">Users</NavLink>
+                </p>
+                <p>
+                  <button onClick={handleLogout}>Logout</button>
+                </p>
+              </>
+            )}
+          </ul>
+        </header>
+      </div>
+    </div>
   );
 };
 export default Navbar;
