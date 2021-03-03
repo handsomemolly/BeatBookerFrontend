b1a2c1974141c1353863d03c213739a012a6a750
handsomemolly <ryan.kendig@gmail.com>
2021-03-02 21:35:32 -0800

    css stinks

--- beatbooker-client/src/components/Login.js
+++ beatbooker-client/src/components/Login.js
@@ -12,28 +12,73 @@ class Login extends React.Component {
   };
   render() {
     return (
-      <div className="Login">
-        <form onSubmit={(e) => this.props.handleLoginOrSignup(e, this.state)}>
-          <label>Username</label>
-          <input
-            type="text"
-            name="username"
-            value={this.state.username}
-            onChange={this.handleChange}
-          />
-          <br />
-          <label>Password</label>
-          <input
-            type="password"
-            name="password"
-            value={this.state.password}
-            onChange={this.handleChange}
-          />
-          <br />
-          <input type="submit" value="Submit" />
-        </form>
+      <div>
+        <div className="login">
+          <div className="heading">
+            <h1>Log In</h1>
+          </div>
+
+          <form
+            action="#"
+            onSubmit={(e) => this.props.handleLoginOrSignup(e, this.state)}
+          >
+            <div className="input-group input-group-lg">
+              <span class="input-group-addon">
+                <i class="fa fa-user"></i>
+              </span>
+              <input
+                type="text"
+                className="form-control"
+                name="username"
+                placeholder="Username"
+                value={this.state.username}
+                onChange={this.handleChange}
+              />
+            </div>
+            <br />
+            <div className="input-group input-group-lg">
+              <span class="input-group-addon">
+                <i class="fa fa-user"></i>
+              </span>
+              <input
+                type="password"
+                className="form-control"
+                name="password"
+                placeholder="password"
+                value={this.state.password}
+                onChange={this.handleChange}
+              />
+            </div>
+            <br />
+            <button type="submit" className="float" value="Submit">
+              Log In
+            </button>
+          </form>
+        </div>
       </div>
     );
   }
 }
 export default Login;
+
+{
+  /* <div class="login">
+  <div class="heading">
+    <h2>Sign in</h2>
+    <form action="#">
+
+      <div class="input-group input-group-lg">
+        <span class="input-group-addon"><i class="fa fa-user"></i></span>
+        <input type="text" class="form-control" placeholder="Username or email">
+          </div>
+
+        <div class="input-group input-group-lg">
+          <span class="input-group-addon"><i class="fa fa-lock"></i></span>
+          <input type="password" class="form-control" placeholder="Password">
+        </div>
+
+        <button type="submit" class="float">Login</button>
+       </form>
+ 		</div>
+ </div> */
+}
